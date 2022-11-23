e6148b1065cf03d53be3889b18154be39a70592b
MatU <mattves@bk.ru>
2022-11-23 21:14:13 +0300

    Задание 3 выполнено

---
+++ Lesson1/Task_3/Task_3.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа принимает на вход число и выдает, является ли оно четным.");
+Console.WriteLine("Введите число: ");
+string inputValue1 = Console.ReadLine();
+int value1 = Convert.ToInt32(inputValue1);
+
+
+if (value1%2 == 0)
+{
+System.Console.WriteLine("да");
+} 
+else
+{
+System.Console.WriteLine("нет");  
+}
+
+
+
