c725ca817f502262fd7b2c5366a960d7a328bb29
MatU <mattves@bk.ru>
2022-11-23 21:22:09 +0300

    Задание 4 выполнено

---
+++ Lesson1/Task_4/Task_4.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа принимает на вход число N и выдает, все четные числа от 1 до N.");
+Console.WriteLine("Введите число: ");
+string inputValue1 = Console.ReadLine();
+int value1 = Convert.ToInt32(inputValue1);
+Console.WriteLine("Результат равен: ");
+for(int i=1;value1>=i;i++){
+    if (i%2==0){
+       Console.Write(i);
+    }
+}
