782f102afcf4d9860075b666d54b0abd55f905b1
MatU <mattves@bk.ru>
2022-12-30 20:42:45 +0300

    created lojik in project

--- Project/Program.cs
+++ Project/Program.cs
@@ -3,6 +3,18 @@
 // задать на старте выполнение алгоритма.
 
 string [] str1 = {"hello", "2","world", "^-)"};
-string [] str2;
+string [] str2 = new string[str1.Length];
+
+for(int i = 0; i < str1.Length; i++){
+    char [] b = str1[i].ToCharArray();
+    if( b.Length <= 3){
+        str2[i] = str1[i];
+    }
+}
+
+for( int j = 0; j < str2.Length;j++){  
+        System.Console.Write($"{str2[j]}  ");
+    }    
+
 
 
